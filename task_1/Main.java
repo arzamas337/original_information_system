51c223191f191bcdf6c8a1e2acd5a381f978f71f
arzamas337 <144003743+arzamas337@users.noreply.github.com>
2025-09-19 18:06:54 +0300

    Update Main.java

--- task_1/Main.java
+++ task_1/Main.java
@@ -1,12 +1,30 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Создание нового клиента");
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
 
         try {
+            System.out.println("Создание клиента");
+            String jsonPath = "C:/Users/user/Desktop/example.json";
+            Client clientFromJson = new Client(jsonPath, true);
+            System.out.println("\nКлиент успешно создан");
+            System.out.println(clientFromJson);
+
+            System.out.println("\nСоздание клиента");
+            String dataString = "ООО_X5;Частная;Москва,ул.Ленина,д.10;89161234567;Иванов Иван Иванович";
+            Client clientFromString = new Client(dataString);
+            System.out.println("\nКлиент успешно создан");
+            System.out.println(clientFromString);
+
+            System.out.println("\nСоздание клиента");
+
             System.out.print("Введите название организации: ");
             String orgName = Client.validateOrganizationName(scanner.nextLine());
 
@@ -19,13 +37,15 @@ public class Main {
             System.out.print("Введите телефон: ");
             String telephone = Client.validateAndNormalizePhone(scanner.nextLine());
 
-            System.out.print("Введите контактное лицо (Фамилия Имя [Отчество]): ");
+            System.out.print("Введите контактное лицо (Фамилия Имя (Отчество)): ");
             String contactPerson = Client.validateContactPerson(scanner.nextLine());
 
-            Client client = new Client(orgName, typeProperty, address, telephone, contactPerson);
-            System.out.println("Клиент успешно создан!");
-            System.out.println(client);
+            Client clientFromConsole = new Client(orgName, typeProperty, address, telephone, contactPerson);
+            System.out.println("\nКлиент успешно создан");
+            System.out.println(clientFromConsole);
 
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + e.getMessage());
         } catch (IllegalArgumentException e) {
             System.out.println("Ошибка: " + e.getMessage());
         } finally {
@@ -33,4 +53,3 @@ public class Main {
         }
     }
 }
-
