70929d4a934b1983ca2fd2dc206cae9f12033c6f
arzamas337 <144003743+arzamas337@users.noreply.github.com>
2025-09-19 18:41:25 +0300

    Update Main.java

--- task_1/Main.java
+++ task_1/Main.java
@@ -14,17 +14,17 @@ public class Main {
             System.out.println("Создание клиента");
             String jsonPath = "C:/Users/user/Desktop/example.json";
             Client clientFromJson = new Client(jsonPath, true);
-            System.out.println("\nКлиент успешно создан");
-            System.out.println(clientFromJson);
+            System.out.println("Клиент успешно создан");
+            System.out.println(clientFromJson.toStringFull());
+            System.out.println(clientFromJson.toStringShort());
 
             System.out.println("\nСоздание клиента");
             String dataString = "ООО_X5;Частная;Москва,ул.Ленина,д.10;89161234567;Иванов Иван Иванович";
             Client clientFromString = new Client(dataString);
-            System.out.println("\nКлиент успешно создан");
-            System.out.println(clientFromString);
+            System.out.println("Клиент успешно создан");
+            System.out.println(clientFromString.toStringFull());
 
             System.out.println("\nСоздание клиента");
-
             System.out.print("Введите название организации: ");
             String orgName = Client.validateOrganizationName(scanner.nextLine());
 
@@ -41,8 +41,15 @@ public class Main {
             String contactPerson = Client.validateContactPerson(scanner.nextLine());
 
             Client clientFromConsole = new Client(orgName, typeProperty, address, telephone, contactPerson);
-            System.out.println("\nКлиент успешно создан");
-            System.out.println(clientFromConsole);
+            System.out.println("Клиент успешно создан");
+            System.out.println(clientFromConsole.toStringFull());
+
+            Client clientA = new Client("ООО_X5", "Частная", "Москва, ул. Ленина, д.10", "89161234567", "Иванов Иван Иванович");
+            Client clientB = new Client("ООО_X5", "Частная", "Москва, ул. Ленина, д.15", "89161234567", "Иванов Иван Иванович");
+            System.out.println("Сравнение clientA и clientB: " + clientA.equals(clientB));
+
+            Client clientC = new Client("ООО_X5", "Частная", "Москва, ул. Ленина, д.10", "89161234568", "Иванов Иван Иванович");
+            System.out.println("Сравнение clientA и clientC: " + clientA.equals(clientC));
 
         } catch (IOException e) {
             System.out.println("Ошибка чтения файла: " + e.getMessage());
