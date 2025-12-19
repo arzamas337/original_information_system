3989fa65775f3e3e359f06822cb1b04c08744a1e
arzamas337 <144003743+arzamas337@users.noreply.github.com>
2025-12-19 16:54:05 +0300

    3.3 Обновление WebAppServer.java

--- task3/WebAppServer.java
+++ task3/WebAppServer.java
@@ -4,8 +4,10 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.example.controller.ClientEditController;
 import org.example.controller.ClientListController;
 import org.example.controller.ClientDetailsController;
+import org.example.controller.ClientAddController;
 
 import java.io.File;
 
@@ -13,38 +15,32 @@ public class WebAppServer {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        ServletContextHandler context =
+                new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
 
         context.addServlet(new ServletHolder(new ClientListController()), "/clients");
         context.addServlet(new ServletHolder(new ClientDetailsController()), "/client");
+        context.addServlet(new ServletHolder(new ClientAddController()), "/client/add");
+        context.addServlet(new ServletHolder(new ClientEditController()), "/client/edit");
+
 
         String projectRoot = System.getProperty("user.dir");
         String staticPath = projectRoot + "/src/main/resources/static";
 
-        System.out.println("Looking for static files in: " + staticPath);
-
         File staticDir = new File(staticPath);
-        if (!staticDir.exists()) {
-            System.err.println("ERROR: Static directory not found: " + staticPath);
-            System.err.println("Current working directory: " + projectRoot);
-            staticPath = projectRoot + "/PIS2/src/main/resources/static";
-            staticDir = new File(staticPath);
-            if (!staticDir.exists()) {
-                System.err.println("ERROR: Alternative path also not found: " + staticPath);
-            }
-        }
 
-        ServletHolder staticHolder = new ServletHolder("default", DefaultServlet.class);
+        ServletHolder staticHolder =
+                new ServletHolder("default", DefaultServlet.class);
         staticHolder.setInitParameter("resourceBase", staticDir.getAbsolutePath());
         staticHolder.setInitParameter("dirAllowed", "true");
         staticHolder.setInitParameter("pathInfoOnly", "true");
+
         context.addServlet(staticHolder, "/static/*");
 
         server.setHandler(context);
         server.start();
-        System.out.println("Server started at http://localhost:8080/clients");
-        System.out.println("CSS should be at: http://localhost:8080/static/style.css");
+        System.out.println("http://localhost:8080/clients");
         server.join();
     }
 }
