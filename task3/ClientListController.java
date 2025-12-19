3ee98a71a2b0b8c665e8ced4bec0afd646b49e49
arzamas337 <144003743+arzamas337@users.noreply.github.com>
2025-12-19 21:04:55 +0300

    7.2 Обновление ClientListController.java

--- task3/ClientListController.java
+++ task3/ClientListController.java
@@ -4,9 +4,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.model.ClientModel;
-import org.example.model.decorator.FilterTypePropertyDecorator;
-import org.example.model.decorator.FilterTypePropertyFileDecorator;
-import org.example.model.decorator.ViewMode;
+import org.example.model.decorator.*;
 import org.example.model.entity.Client;
 import org.example.model.factory.DataSourceType;
 import org.example.model.factory.RepositoryFactory;
@@ -21,30 +19,36 @@ public class ClientListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html;charset=UTF-8");
-        
-        String sourceParam = req.getParameter("source");
-        if (sourceParam == null) sourceParam = "JSON";
-        DataSourceType source = DataSourceType.valueOf(sourceParam);
-        
-        String viewParam = req.getParameter("view");
-        if (viewParam == null) viewParam = "NONE";
-        ViewMode viewMode = ViewMode.valueOf(viewParam);
+
+        DataSourceType source = DataSourceType.valueOf(
+                req.getParameter("source") == null ? "JSON" : req.getParameter("source")
+        );
+
+        ViewMode viewMode = ViewMode.valueOf(
+                req.getParameter("view") == null ? "NONE" : req.getParameter("view")
+        );
+
+        SortMode sortMode = SortMode.valueOf(
+                req.getParameter("sort") == null ? "NONE" : req.getParameter("sort")
+        );
 
         MyClientRep repository = RepositoryFactory.create(source);
         
         switch (viewMode) {
-            case PRIVATE ->
-                    repository = new FilterTypePropertyDecorator(repository);
-            case STATE ->
-                    repository = new FilterTypePropertyFileDecorator(repository);
-            default -> { }
+            case PRIVATE -> repository = new FilterTypePropertyDecorator(repository);
+            case STATE -> repository = new FilterTypePropertyFileDecorator(repository);
+        }
+        
+        switch (sortMode) {
+            case NAME_ASC -> repository = new SortOrganizationNameDecorator(repository);
+            case NAME_DESC -> repository = new SortOrganizationNameFileDecorator(repository);
         }
 
         ClientModel model = new ClientModel(repository);
 
         try {
             List<Client> clients = model.getAllClients();
-            String html = ClientListView.render(clients, source, viewMode);
+            String html = ClientListView.render(clients, source, viewMode, sortMode);
             resp.getWriter().print(html);
         } catch (Exception e) {
             resp.getWriter().print("Ошибка: " + e.getMessage());
